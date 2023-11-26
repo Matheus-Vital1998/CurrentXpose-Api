33d7ae3c35ebbbc809e06aca14163db87234ee68
Matheus Miranda <matheus.britomiranda7308@gmail.com>
2023-11-26 20:25:19 -0300

    [FEAT] - Implementando filtros de leitura

--- CurrentXposeAPI/Services/Interfaces/ILeituraService.cs
+++ CurrentXposeAPI/Services/Interfaces/ILeituraService.cs
@@ -5,5 +5,15 @@ namespace CurrentXposeAPI.Services.Interfaces
     public interface ILeituraService
     {
         Task<IReadOnlyList<Leitura>> ObterDadosDeLeitura();
+
+        Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Morador(string loginMorador, DateTime dataIncial, DateTime dataFinal);
+
+        Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroResidencia, string nomePredio);
+
+        Task<IReadOnlyList<Leitura>> ObterLeiturasPorAndar_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroAndar, string nomePredio);
+
+        Task<IReadOnlyList<Leitura>> ObterLeiturasPorPredio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string nomePredio);
+
+        Task<IReadOnlyList<Leitura>> ObterLeiturasPorCondominio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal);
     }
 }
--- CurrentXposeAPI/Services/LeituraService.cs
+++ CurrentXposeAPI/Services/LeituraService.cs
@@ -17,5 +17,30 @@ namespace CurrentXposeAPI.Services
         {
             return _leituraRepository.GetAll();
         }
+
+        public Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Morador(string loginMorador, DateTime dataIncial, DateTime dataFinal)
+        {
+            return _leituraRepository.ObterLeiturasPorMorador_Morador(loginMorador, dataIncial, dataFinal);
+        }
+
+        public Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroResidencia, string nomePredio)
+        {
+            return _leituraRepository.ObterLeiturasPorMorador_Sindico(loginSindico, dataIncial, dataFinal, numeroResidencia, nomePredio);
+        }
+
+        public Task<IReadOnlyList<Leitura>> ObterLeiturasPorAndar_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroAndar, string nomePredio)
+        {
+            return _leituraRepository.ObterLeiturasPorAndar_Sindico(loginSindico, dataIncial, dataFinal, numeroAndar, nomePredio);
+        }
+
+        public Task<IReadOnlyList<Leitura>> ObterLeiturasPorPredio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string nomePredio)
+        {
+            return _leituraRepository.ObterLeiturasPorPredio_Sindico(loginSindico, dataIncial, dataFinal, nomePredio);
+        }
+
+        public Task<IReadOnlyList<Leitura>> ObterLeiturasPorCondominio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal)
+        {
+            return _leituraRepository.ObterLeiturasPorCondominio_Sindico(loginSindico, dataIncial, dataFinal);
+        }
     }
 }
