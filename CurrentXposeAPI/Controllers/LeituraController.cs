bb97d1d0c0f920121e30140ecb13c755c31a9315
Matheus Oliveira <fh185@t-systems.com.br>
2023-11-26 21:09:15 -0300

    [FIX] - corrigindo erros na api

--- CurrentXposeAPI/Controllers/LeituraController.cs
+++ CurrentXposeAPI/Controllers/LeituraController.cs
@@ -25,45 +25,5 @@ namespace CurrentXposeAPI.Controllers
             var leitura = await _leituraService.ObterDadosDeLeitura();
             return leitura.ToList();
         }
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("ObterLeiturasPorMorador_Morador")]
-        public async Task<List<Leitura>> ObterLeiturasPorMorador_Morador(string loginMorador, DateTime dataIncial, DateTime dataFinal)
-        {
-            var leitura = await _leituraService.ObterLeiturasPorMorador_Morador(loginMorador, dataIncial, dataFinal);
-            return leitura.ToList();
-        }
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("ObterLeiturasPorMorador_Morador")]
-        public async Task<List<Leitura>> ObterLeiturasPorMorador_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroResidencia, string nomePredio)
-        {
-            var leitura = await _leituraService.ObterLeiturasPorMorador_Sindico(loginSindico, dataIncial, dataFinal, numeroResidencia, nomePredio);
-            return leitura.ToList();
-        }
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("ObterLeiturasPorMorador_Morador")]
-        public async Task<List<Leitura>> ObterLeiturasPorAndar_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroAndar, string nomePredio)
-        {
-            var leitura = await _leituraService.ObterLeiturasPorAndar_Sindico(loginSindico, dataIncial, dataFinal, numeroAndar, nomePredio);
-            return leitura.ToList();
-        }
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("ObterLeiturasPorMorador_Morador")]
-        public async Task<List<Leitura>> ObterLeiturasPorPredio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string nomePredio)
-        {
-            var leitura = await _leituraService.ObterLeiturasPorPredio_Sindico(loginSindico, dataIncial, dataFinal, nomePredio);
-            return leitura.ToList();
-        }
-
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [HttpGet("ObterLeiturasPorMorador_Morador")]
-        public async Task<List<Leitura>> ObterLeiturasPorCondominio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal)
-        {
-            var leitura = await _leituraService.ObterLeiturasPorCondominio_Sindico(loginSindico, dataIncial, dataFinal);
-            return leitura.ToList();
-        }
     }
 }
--- CurrentXposeAPI/Repository/Interfaces/ILeituraRepository.cs
+++ CurrentXposeAPI/Repository/Interfaces/ILeituraRepository.cs
@@ -5,15 +5,5 @@ namespace CurrentXposeAPI.Repository.Interfaces
     public interface ILeituraRepository : IBaseRepository<Leitura>
     {
         Task<IReadOnlyList<Leitura>> GetAll();
-
-        Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Morador(string loginMorador, DateTime dataIncial, DateTime dataFinal);
-
-        Task<IReadOnlyList<Leitura>> ObterLeiturasPorMorador_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroResidencia, string nomePredio);
-
-        Task<IReadOnlyList<Leitura>> ObterLeiturasPorAndar_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string numeroAndar, string nomePredio);
-
-        Task<IReadOnlyList<Leitura>> ObterLeiturasPorPredio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal, string nomePredio);
-
-        Task<IReadOnlyList<Leitura>> ObterLeiturasPorCondominio_Sindico(string loginSindico, DateTime dataIncial, DateTime dataFinal);
     }
 }
